a58000f926d36e0c4770444ac8b2f9c353063f47
scallopy <scallopy@abv.bg>
2019-05-18 08:18:35 +0300

    Using Subject to activate user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { UsersService } from '../users.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,7 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
     this.route.params
@@ -18,6 +23,10 @@ export class UserComponent implements OnInit {
           this.id = +params['id'];
         }
       );
+  }
+
+  onActivate() {
+    this.usersService.userActivated.next(this.id);
   }
 
 }
